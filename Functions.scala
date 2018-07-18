e90e48dc241f30a80b79363edcc323c08da5f54e
nitin684 <foxnitin684@gmail.com>
2018-07-18 21:09:21 +0530

    Update Functions.scala

--- Functions.scala
+++ Functions.scala
@@ -1,4 +1,5 @@
 
+
 object Functions {
   
 /* Syntax to declare a scala Function 
@@ -28,31 +29,38 @@ object Functions {
       println("Result1: " + result4 +"\n"+ "Result2: " + result5 +"\n"+ "Result3: " + result6)
       
       higherOrderfuncEx(25, multiplyBy2)        // Passing a function as parameter  
+      
+      /* Function Composition */
+      var result = multiplyBy2(add2(10)) 
+      println("*****Result of Function Composition*****" +"\n"+ result)
+      
+      println(defaultfuncEx(20,multiplyBy2(3)))
   }  
+  
   /* Non-Parameterized Function */
   def functionExample()  {          
-      println("This is a simple function")  
+	    println("This is a simple function")  
   }
   /* Function with = operator */
   def retfunctionExample():Any = {       
-      var a:Any = 10  
-       a  
+		  var a:Any = 10  
+				  a  
   }  
   /* Parameterized Function */
   def functionExample(a:Int, b:Int) = {  
-      var c = a+b  
-      println("*****Parametrized function invoked*****" +"\n"+ c)  
+		  var c = a+b  
+		  println("*****Parametrized function invoked*****" +"\n"+ c)  
   }  
   /* Recursion Function */
   def recursionfuncEx(a:Int, b:Int):Int = {  
-      if(b == 0)          // Base condition  
-	0  
-      else  
-	a + recursionfuncEx(a,b-1)  
+		  if(b == 0)  // Base condition  
+			  0  
+		  else  
+			  a + recursionfuncEx(a,b-1)  
   }  
   /* Parameters with default values as 0 */
   def defaultfuncEx(a:Int = 0, b:Int = 0):Int = {     
-      a+b  
+		  a+b  
   }
   /* Function With Named Parameter */
   def namedparamfuncEx(a:Int, b:Int):Int = {  
@@ -61,9 +69,16 @@ object Functions {
   /* Higher Order Functions */
   def higherOrderfuncEx(a:Int, f:Int=>AnyVal):Unit = {  
       println("*****Result of Higher order function*****" +"\n"+ f(a))   
-  }  
+  }
+  
   def multiplyBy2(a:Int):Int = {  
       a*2  
+  }
+  
+  def add2(a:Int):Int = {  
+      a+2  
   }  
+  
+  
   
 }
