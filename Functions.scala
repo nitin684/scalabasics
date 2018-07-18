4cb4312c2601b67ccec5012cb1b7dcb0fedca227
nitin684 <foxnitin684@gmail.com>
2018-07-18 20:22:33 +0530

    Update Functions.scala

--- Functions.scala
+++ Functions.scala
@@ -1,13 +1,5 @@
 
-
 object Functions {
-  
-  def main(args: Array[String]) {  
-      functionExample()           // Calling function 
-      println(retfunctionExample)
-      functionExample(2,4)
-      println(recursionfuncEx(9,6))
-  }  
   
 /* Syntax to declare a scala Function 
  * 
@@ -15,26 +7,64 @@ object Functions {
       //statements to be executed  
 	}  
  */
- 
-  def functionExample()  {        // Defining a non parameterized function  
-      println("This is a simple function")  
+  
+  def main(args: Array[String]) { 
+    
+      functionExample()            
+      println("*****Return from function*****" +"\n"+ retfunctionExample)
+      functionExample(2,4)
+      println("*****Return from Recursion function*****" +"\n"+ recursionfuncEx(9,6))
+      
+      var result1 = defaultfuncEx(15,2) // Calling with two values  
+      var result2 = defaultfuncEx(15)   // Calling with one value  
+      var result3 = defaultfuncEx()     // Calling without any value  
+      println("*****Function with default parameter definition invoked*****")
+      println("Result1: " + result1 +"\n"+ "Result2: " + result2 +"\n"+ "Result3: " + result3)  
+      
+      var result4 = namedparamfuncEx(a = 15, b = 2)    // Parameters names are passed during call  
+      var result5 = namedparamfuncEx(b = 15, a = 2)    // Parameters order have changed during call  
+      var result6 = namedparamfuncEx(15,2)             // Only values are passed during call  
+      println("*****Function with Named parameter definition invoked*****")
+      println("Result1: " + result4 +"\n"+ "Result2: " + result5 +"\n"+ "Result3: " + result6)
+      
+      higherOrderfuncEx(25, multiplyBy2)        // Passing a function as parameter  
+  }  
+  
+  /* Non-Parameterized Function */
+  def functionExample()  {          
+	    println("This is a simple function")  
   }
-  
-  def retfunctionExample():Any = {       // Defining a function  
-      var a:Any = 10  
-      a  
+  /* Function with = operator */
+  def retfunctionExample():Any = {       
+		  var a:Any = 10  
+				  a  
   }  
-
+  /* Parameterized Function */
   def functionExample(a:Int, b:Int) = {  
-      var c = a+b  
-      println(c)  
+		  var c = a+b  
+		  println("*****Parametrized function invoked*****" +"\n"+ c)  
+  }  
+  /* Recursion Function */
+  def recursionfuncEx(a:Int, b:Int):Int = {  
+		  if(b == 0)          // Base condition  
+			  0  
+		  else  
+			  a + recursionfuncEx(a,b-1)  
+  }  
+  /* Parameters with default values as 0 */
+  def defaultfuncEx(a:Int = 0, b:Int = 0):Int = {     
+		  a+b  
+  }
+  /* Function With Named Parameter */
+  def namedparamfuncEx(a:Int, b:Int):Int = {  
+      a/b 
+  } 
+  /* Higher Order Functions */
+  def higherOrderfuncEx(a:Int, f:Int=>AnyVal):Unit = {  
+      println("*****Result of Higher order function*****" +"\n"+ f(a))   
+  }  
+  def multiplyBy2(a:Int):Int = {  
+      a*2  
   }  
   
-  def recursionfuncEx(a:Int, b:Int):Int = {  
-      if(b == 0)          // Base condition  
-       0  
-      else  
-       a + recursionfuncEx(a,b-1)  
-  }  
-
 }
