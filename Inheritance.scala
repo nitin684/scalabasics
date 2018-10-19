432f31713ef70d48c0a55362a7d3a01daee22585
nitin684 <foxnitin684@gmail.com>
2018-10-19 18:13:36 +0530

    Create Inheritance.scala

---
+++ Inheritance.scala
@@ -0,0 +1,47 @@
+/*
+ * REPL - READ, Evaluate, Print, Loop
+ * Inheritence
+ * 
+ * */
+
+class ThisExample{  
+    var id:Int = 0  
+    var name: String = ""  
+    def this(id:Int, name:String){  
+        this()  
+        this.id = id  
+        this.name = name  
+    }  
+    def show(){  
+        println(id+" "+name)  
+    }  
+}  
+
+class Stud(name:String){  
+    def this(name:String, age:Int){  
+        this(name)  
+        println(name+" "+age)  
+    }      
+} 
+
+abstract class Bike{  
+    def run()  
+}  
+  
+class Hero extends Bike{  
+    def run(){  
+        println("running fine...")  
+    }  
+} 
+  
+object MainObject3 {  
+    def main(args:Array[String]){  
+        var t = new ThisExample(101,"Martin")  
+        t.show()
+        
+        var s = new Stud("Rama",100)  
+        
+        var h = new Hero()  
+        h.run()         
+    }  
+} 
